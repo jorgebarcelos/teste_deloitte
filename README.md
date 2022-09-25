f6ce7751146ba4ffa224419f9ec1fb617838d46a
jorgebarcelos <jorgebarcelos.faria@gmail.com>
2022-09-25 19:43:41 -0300

    implemantação de testes, readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# teste_deloitte
+# instruções
+
+criar viltualenv e instalar o arquivo requirements.txt
+iniciar o servidor django e realizar as operações via postman
+
+É necessario realizar o login na url Token para receber o token access, somente assim as operaçẽs são permitidas.
+username = admin
+password = admin
+
+http://127.0.0.1:8000/escola/token/
+http://127.0.0.1:8000/escola/aluno/
+http://127.0.0.1:8000/escola/disciplina/
+http://127.0.0.1:8000/escola/boletim/
+http://127.0.0.1:8000/escola/notas/
+
+Para acessar a documentação é necessário acessar a url do swagger ou redoc
+http://127.0.0.1:8000/escola/swagger/
+http://127.0.0.1:8000/escola/redoc/
--- teste_pytest.py
+++ teste_pytest.py
@@ -4,7 +4,7 @@ import requests
 class TestEscola:
     payload = {"username": "admin", "password": "admin"}
     headers = {
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY0MTIzNzQ1LCJpYXQiOjE2NjQxMjM0NDUsImp0aSI6ImU5OWExZmMwYjdmMTRlYmQ4ZmI5OTdkZjc5ZTVjMjIzIiwidXNlcl9pZCI6Mn0.kmOC-DHDx8T635MYlT0D-WCtnvEM4aHJsgpLDwcbL5k"
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY0MTI1MTQ0LCJpYXQiOjE2NjQxMjQ4NDQsImp0aSI6IjFlYTFiNWJiNDdhNjRkMTI4NjcxOTNjZjBjZWUxMTVkIiwidXNlcl9pZCI6Mn0.Soqwmr6MrrGbafl9I-PhM_uI6I7AF0swbuJbiFESiNI"
     }
     url_login = "http://127.0.0.1:8000/escola/token/"
     url_escola = "http://127.0.0.1:8000/escola/"
@@ -34,8 +34,18 @@ class TestEscola:
 
 
     def test_deleta_aluno(self):
-        id = 6
+        id = 4
         exclui_aluno = requests.delete(url=f'{self.url_aluno}{id}', headers=self.headers)
         assert exclui_aluno.status_code == 204
 
 
+    def test_put_aluno(self):
+        id = 4
+        payload = {
+            "nome": "Andre Andrade",
+            "email": "andre@andre.com",
+            "data_nascimento": "1985-01-12"
+        }
+
+        altera_aluno = requests.post(url=f'{self.url_aluno}{id}', headers=self.headers, json=payload)
+        assert altera_aluno.status_code == 200
